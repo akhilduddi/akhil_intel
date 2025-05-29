1e34d0ee5c67d89f1033474c16f597cb7aac5f30
akhil <akhilduddi95@mail@example.com>
2025-05-29 23:01:58 +0530

    api

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base:process.env.VITE_GEMINI_API_KEY || "/akhil_intel",
   envPrefix: 'VITE_',
   server: {
     port: 3000,
